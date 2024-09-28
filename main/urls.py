680dd5c398be779bf562640f3c279722e4f2071a
Bona3121 <marlamarlena999@gmail.com>
2024-09-28 19:16:45 +0700

    masih error

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from main.views import show_main, create_food_entry, show_xml,  show_json,  show_xml_by_id, show_json_by_id
-from django.contrib import admin
+from main.views import show_main
 
 app_name = 'main'
 
-urlpatterns = [
+urlpatterns = [ 
     path('', show_main, name='show_main'),
-    path('create-food-entry', create_food_entry, name='create_food_entry'),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
-    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
