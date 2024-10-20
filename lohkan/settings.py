acf8d699654d1f8a902020b0e8314e8878fee9cd
jasminerakhaila <jrakhaila@gmail.com>
2024-10-20 21:00:35 +0700

    Tambah model, urls, views, page_review.html

--- lohkan/settings.py
+++ lohkan/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "main",
     "authentication",
+    "food_review",
 ]
 
 MIDDLEWARE = [
--- lohkan/urls.py
+++ lohkan/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls')),
     path('auth/', include('authentication.urls')),
+    path('food-review/', include('food_review.urls')),
 ]
