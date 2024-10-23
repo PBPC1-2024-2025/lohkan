6698635c0d10851500abd275f89a5537319e523b
fakhrihabb <fakhrihabb@gmail.com>
2024-10-23 14:15:02 +0700

    penambahan forms

--- explore/urls.py
+++ explore/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from explore.views import show_explore
+from explore.views import show_explore, add_food
 
 app_name = 'explore'
 
 urlpatterns = [
     path('', show_explore, name='show_explore'),
+    path('add_food/', add_food, name='add_food'),
 ]
--- explore/views.py
+++ explore/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
+from explore.forms import FoodForm
+from explore.models import Food
 
 def show_explore(request):
+    foods = Food.objects.all()
+
     context = {
         'test': 'test',
+        'foods': foods,
     }
 
     return render(request, "explore.html", context)
+
+def add_food(request):
+    form = FoodForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('explore:show_explore')
+    context = {'form': form}
+    return render(request, "add_food.html", context)
