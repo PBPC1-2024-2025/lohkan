36417341722cd32d6ff8a97b90e4dbebfb506c02
fakhrihabb <fakhrihabb@gmail.com>
2024-10-23 13:56:41 +0700

    reset, start from beginning

--- explore/views.py
+++ explore/views.py
@@ -1,21 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 
-from explore.models import Food
+def show_main(request):
+    context = {
+        'npm' : '2306123456',
+        'name': 'Pak Bepe',
+        'class': 'PBP E'
+    }
 
-
-@csrf_exempt
-@require_POST
-def add_food_ajax(request):
-    name = request.POST.get('name')
-    description = request.POST.get('description')
-    min_price = request.POST.get('min_price')
-    max_price = request.POST.get('max_price')
-
-    new_food = Food(name=name, description=description, min_price=min_price, max_price=max_price)
-    new_food.save()
-    return HttpResponse(b"CREATED", status= 201)
-
-
+    return render(request, "main.html", context)
--- lohkan/urls.py
+++ lohkan/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls')),
     path('auth/', include('authentication.urls')),
+    path('', include('explore.urls'))
 ]
