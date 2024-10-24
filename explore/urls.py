da314ec6026e3b8abc23d04927b79e8a6bf88012
fakhrihabb <fakhrihabb@gmail.com>
2024-10-24 20:30:03 +0700

    debugging agar list makanan muncul

--- explore/urls.py
+++ explore/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from explore.views import show_explore, add_food, edit_food
+from explore.views import show_explore, add_food, edit_food, delete_food, show_xml
 
 app_name = 'explore'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('explore', show_explore, name='show_explore'),
     path('add_food/', add_food, name='add_food'),
     path('edit_food/<uuid:id>', edit_food, name='edit_food'),
+    path('delete_food/<uuid:id>', delete_food, name='delete_food'),
+    path('xml/', show_xml, name='show_xml'),
 ]
--- explore/views.py
+++ explore/views.py
@@ -1,8 +1,9 @@
 from lib2to3.fixes.fix_input import context
 
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect, reverse
+from django.core import serializers
 
 from explore.forms import FoodForm
 from explore.models import Food
@@ -10,16 +11,13 @@ from explore.models import Food
 @login_required(login_url='auth/login')
 def show_explore(request):
     foods = Food.objects.all()
-
     context = {
         'foods': foods,
     }
-
     return render(request, "explore.html", context)
 
 def add_food(request):
     form = FoodForm(request.POST or None)
-
     if form.is_valid():
         form.save()
         return redirect('explore:show_explore')
@@ -34,3 +32,12 @@ def edit_food(request, id):
         return HttpResponseRedirect(reverse('explore:show_explore'))
     context = {'form': form}
     return render(request, "edit_food.html", context)
+
+def delete_food(request, id):
+    food = Food.objects.get(pk=id)
+    food.delete()
+    return HttpResponseRedirect(reverse('explore:show_explore'))
+
+def show_xml(request):
+    data = Food.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
