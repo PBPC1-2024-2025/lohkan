88149f41b455e4e9cc997a0e610af9f72b47e792
fakhrihabb <fakhrihabb@gmail.com>
2024-10-24 20:10:04 +0700

    routing url

--- explore/urls.py
+++ explore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from explore.views import show_explore, add_food
+from explore.views import show_explore, add_food, edit_food
 
 app_name = 'explore'
 
 urlpatterns = [
     path('explore', show_explore, name='show_explore'),
     path('add_food/', add_food, name='add_food'),
+    path('edit_food/<uuid:id>', edit_food, name='edit_food'),
 ]
