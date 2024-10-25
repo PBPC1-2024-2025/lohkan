808210c06dfc402d96f6f80770a646502c57c259
fakhrihabb <fakhrihabb@gmail.com>
2024-10-25 11:28:25 +0700

    penambahan type ke forms

--- explore/forms.py
+++ explore/forms.py
@@ -4,4 +4,4 @@ from explore.models import Food
 class FoodForm(ModelForm):
     class Meta:
         model = Food
-        fields = ["name", "description", "min_price", "max_price", "image_link"]
+        fields = ["name", "description", "min_price", "max_price", "image_link", "type"]
