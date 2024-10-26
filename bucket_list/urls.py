09446359bb7f3bcffd521fcb644a0822c3c09471
fakhrihabb <fakhrihabb@gmail.com>
2024-10-27 04:21:43 +0700

    menambahkan fitur filter dan search berbarengan

--- bucket_list/urls.py
+++ bucket_list/urls.py
@@ -4,7 +4,7 @@ from bucket_list.views import show_bucket_list, delete_bucket_list, edit_bucket_
 app_name = 'bucket_list'
 
 urlpatterns = [
-    path('', show_bucket_list, name='show_bucket_list'),
+    path('show-bucket-list', show_bucket_list, name='show_bucket_list'),
     path('history', show_bucket_list_history, name='show_bucket_list_history'),
     path('add-bucket-list', add_bucket_list, name='add_bucket_list'),
     path('delete-bucket-list/<uuid:id>', delete_bucket_list, name='delete_bucket_list'),
