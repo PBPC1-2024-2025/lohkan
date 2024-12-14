062b9df7f52770a9201e264333c2b48e518556c7
Bona3121 <marlamarlena999@gmail.com>
2024-12-14 21:25:27 +0700

    change media url to online

--- lohkan/settings.py
+++ lohkan/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Quick-start development settings - unsuitable for production
