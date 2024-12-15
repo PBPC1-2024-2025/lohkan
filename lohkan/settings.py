ac3af23bc74d8d5fedff0e4dbb1a3e8c2525d7d5
Bona3121 <marlamarlena999@gmail.com>
2024-12-15 18:22:45 +0700

    ganti url

--- lohkan/settings.py
+++ lohkan/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+# MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = 'marla-marlena-lohkan.pbp.cs.ui.ac.id/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Quick-start development settings - unsuitable for production
